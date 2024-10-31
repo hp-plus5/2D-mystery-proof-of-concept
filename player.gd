0e3e2c419e158cfe3444004d050a9716a36915a3
Sam Warner <its.sam.warner@gmail.com>
2024-10-31 17:46:57 -0400

    Player can take keyboard input to move, but is not animated

--- player.gd
+++ player.gd
@@ -1,16 +1,22 @@
 class_name Player
 extends CharacterBody2D
 
-const SPEED = 150.0
+const MAX_SPEED = 80.0
+const ACCELERATION = 500
+const FRICTION = 500
 
 ## @tutorial(Movement Code at 6:22): https://www.youtube.com/watch?v=TQKXU7iSWUU&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&index=3
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# This came basically premade. For a better version, see tutorial link above.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	# 3.2 way?
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+	move_and_collide(velocity * delta)
